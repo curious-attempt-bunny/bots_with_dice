e2beb89a3328a2afddcf469d46c512502b29587b
Merlyn <curious.attempt.bunny@gmail.com>
2014-06-06 00:13:08 -0700

    Added helpful comments to MyBot.

--- my_bot.rb
+++ my_bot.rb
@@ -1,4 +1,8 @@
 class MyBot < Bot
+	# our_hp             - the number of hit points we have
+	# their_hp           - the number of hit points they have
+	# rolls              - the number of rolls so far to accumulate this attack damage
+	# accumulated_damage - the accumulated damage that can be dealt instead of rolling
   def roll?(our_hp, their_hp, rolls, accumulated_damage)
     # TODO your fun logic goes here!
   end
